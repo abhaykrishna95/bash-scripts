b34c21305e6008a2fbb4123d6489508f15d4c1db
abhaykrishna95 <89457775+abhaykrishna95@users.noreply.github.com>
2022-12-27 00:19:22 +0530

    seconds 60

--- initiate-guacamole.sh
+++ initiate-guacamole.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update -y 
-echo "Hi, I'm sleeping for 40 seconds..."
-sleep 40s 
+echo "Hi, I'm sleeping for 60 seconds..."
+sleep 60
 sudo docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --mysql > initdb.sql
 sudo docker cp initdb.sql mysqldb:/guac_db.sql
 sudo docker exec -it mysqldb sh -c 'cat guac_db.sql | mysql -uroot -p"$MYSQL_ROOT_PASSWORD" guacamole_db'
